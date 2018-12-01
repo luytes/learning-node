0ad9d476b9942ed42679842e7e577663ec107d32
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-01 22:58:17 +0100

    explored function of isString and uniq

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ console.log('Starting app.js');
 const fs = require('fs');
 //gets all the fs modules
 const os = require('os');
-const notes = require('./notes.js')
+const notes = require('./notes.js');
+const _ = require('lodash');
 
 var res = notes.addNote();
 
 var add = notes.add(1,2);
 console.log(add);
+
+console.log(_.isString(true));
+console.log(_.isString('Kevin '));
+
+var filteredArray = _.uniq(['Kevin', 1, 'Kevin', 1, 2, 3, 4]);
+console.log(filteredArray);
+// uniq removes all duplicants in an array
 // var user = os.userInfo();
 
 // fs.appendFileSync('greetings.txt',`Hello ${user.username}! You are ${notes.age}`)
