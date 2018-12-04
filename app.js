80854f39bad916d3e07ca2d9af27d8097954a5d6
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-04 02:34:26 +0100

    learned to use command line arguments to pass data into oour app

--- app.js
+++ app.js
@@ -2,22 +2,40 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 //gets all the fs modules
-const os = require('os');
+// const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
 
-var res = notes.addNote();
+// var res = notes.addNote();
 
-var add = notes.add(1,2);
-console.log(add);
+// var add = notes.add(1,2);
+// console.log(add);
 
-console.log(_.isString(true));
-console.log(_.isString('Kevin '));
+// console.log(_.isString(true));
+// console.log(_.isString('Kevin '));
 
-var filteredArray = _.uniq(['Kevin', 1, 'Kevin', 1, 2, 3, 4]);
-console.log(filteredArray);
+// var filteredArray = _.uniq(['Kevin']);
+// console.log(filteredArray);
 // uniq removes all duplicants in an array
 // var user = os.userInfo();
 
 // fs.appendFileSync('greetings.txt',`Hello ${user.username}! You are ${notes.age}`)
 //tries to append to a file, if it doesnt exist it just creates it with the text Hello World
+
+var command = process.argv[2];
+console.log(process.argv); // array of all the command line arguments passed in
+console.log('Command: ', command);
+
+if (command == 'add') {
+  console.log('Adding new note');
+} else if (command == 'list') {
+  console.log('Listing all notes');
+} else if (command == 'read') {
+  console.log('Reading note');
+} else if (command == 'remove') {
+  console.log('Removing note');
+} else {
+  console.log('Note recognized');
+}
+
+
--- notes.js
+++ notes.js
@@ -4,9 +4,9 @@ module.exports.addNote = () => {
   console.log('addNote');
   return 'New Note';
 }
-module.exports.add = (x,y) => {
-  console.log('Adding those numbers...');
-  // var sum = x + y;
-  // return sum;
-  return x + y
-}
+// module.exports.add = (x,y) => {
+//   console.log('Adding those numbers...');
+//   // var sum = x + y;
+//   // return sum;
+//   return x + y
+// }
