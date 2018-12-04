c18ef6b3b25ad0da3ab07ef76c79d56a5a1c6507
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-04 02:58:51 +0100

    parsing much easier done with yargs, puts it onto a simple object we can access. we use argv objects to grab title, object

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 // const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 // var res = notes.addNote();
 
@@ -22,18 +23,22 @@ const _ = require('lodash');
 // fs.appendFileSync('greetings.txt',`Hello ${user.username}! You are ${notes.age}`)
 //tries to append to a file, if it doesnt exist it just creates it with the text Hello World
 
+const argv = yargs.argv; //
+
 var command = process.argv[2];
 console.log(process.argv); // array of all the command line arguments passed in
 console.log('Command: ', command);
+// console.log('Process: ', process.argv);
+console.log('Yargs :', argv)
 
 if (command == 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command == 'list') {
-  console.log('Listing all notes');
+  notes.getAll(); // create the getAll function in notes.js
 } else if (command == 'read') {
-  console.log('Reading note');
+  notes.getNote(argv.title);
 } else if (command == 'remove') {
-  console.log('Removing note');
+  notes.removeNote(argv.title);
 } else {
   console.log('Note recognized');
 }
--- notes.js
+++ notes.js
@@ -1,9 +1,33 @@
 console.log('starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New Note';
+var addNote = (title, body) => { //anonymous error function
+  console.log('Adding Note:', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes:');
+};
+
+var getNote = (title) => {
+  console.log('Getting note:', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note:', title);
+};
+
+module.exports = {
+  addNote, // same as addNote: addNote
+  getAll,
+  getNote,
+  removeNote
 }
+
+// in Terminal enter: node app.js add --title="secrets" --body="This is my secret"
+// module.exports.addNote = () => {
+//   console.log('addNote');
+//   return 'New Note';
+// }
 // module.exports.add = (x,y) => {
 //   console.log('Adding those numbers...');
 //   // var sum = x + y;
