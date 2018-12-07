148f78a1631a3eaddf762b673c4819ce01610a23
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-08 00:42:51 +0100

    Implemented debugging via command line

--- notes.js
+++ notes.js
@@ -53,6 +53,7 @@ var removeNote = (title) => {
 // DRY DON'T REPEAT YOURSELF
 
 var logNote = (note) => {
+  debugger;
   console.log('--');
   console.log(`Title: ${note.title}`);
   console.log(`Body: ${note.body}`);
---
+++ playground/debugging.js
@@ -0,0 +1,18 @@
+var person = {
+  name: 'Kevin'
+};
+
+person.age = 25;
+
+debugger; // get to specific place through debugger
+
+person.name = 'Felix';
+
+console.log(person);
+
+// HOW TO DEBUG
+// WRITE NODE INSPECT AND THEN THE FILE NAME
+
+// n = next command
+// c = continue through all code
+// repl = explore variable values, manipulate them, mess around with js function
