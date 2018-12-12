fb4a9e4852b489008c35ea60e42e0e42de5a327c
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-12 23:01:24 +0100

    Access to specific information in the object of the http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,6 +7,8 @@ const key = require('./constants.js');
 request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301+lombard+st+philadelphia&key=' + key.constantapi(),
   json: true
-}, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+}, (error, response, body) => { // part of http, request that comes back is the body
+  console.log(`Adress: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
