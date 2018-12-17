0920280262a2e5829110106cea6890401a002470
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-17 16:50:37 +0100

    basics of handlebars to create static pages

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,24 +1,37 @@
 const express = require('express');
+const hbs = require('hbs');
 // create app, call this method:
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+// set various express related configurations
+app.set('view engine', 'hbs');
+
 // setting up all http rout handlers. register a handler
+// router to / == root
 app.get('/', (request, response) => {
   // response.send('Testing express'); // sending for example a https response
-  response.send({
-    name: 'Kevin',
-    age: 25,
-    likes: [
-    'coding',
-    'Yume'
-    ]
+  // response.send({
+  //   name: 'Kevin',
+  //   age: 25,
+  //   likes: [
+  //   'coding',
+  //   'Yume'
+  //   ]
+  // });
+  response.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Hello there'
   });
 });
 
 app.get('/about', (request, response) => {
-  response.send('Testing express, about page');
+  // response.send('Testing express, about page')
+  response.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.listen(3000, () => {
