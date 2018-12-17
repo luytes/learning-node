8a9524f84e8e5ab147898e2daa9127f103dd7510
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-17 23:03:57 +0100

    setup start script and heroku port

--- node-webserver/server.js
+++ node-webserver/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 // create app, call this method:
 var app = express();
 // takes directory you wanna use for all of your partial files, specify it
+// how to register middleware:
+// 1. set up express static directory
 hbs.registerPartials(__dirname + '/views/partials');
 
-app.use(express.static(__dirname + '/public'));
+// then set up logger
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now} ${request.method} ${request.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
 
+// app.use((request, response, next) => {
+//   response.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -43,6 +63,14 @@ app.get('/about', (request, response) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.get('/maintenance', (request, response) => {
+  response.render('maintenance.hbs', {
+    pageTitle: 'maintenance!',
+  })
+})
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 }); // local host 3000
+
+// heroku tries to start the app, but it doesnt know what the filename is called. intead its starting the "start" in package.json script!
