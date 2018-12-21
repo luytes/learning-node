a51882663c29faedc90cca2712342c784d7c0f1d
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-21 22:32:50 +0100

    testing for express apps available

--- node-testing/server/server.js
+++ node-testing/server/server.js
@@ -4,8 +4,25 @@ var app = express();
 
 // http get handler
 app.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.status(404).send({
+    error: 'Page not found',
+    name: 'Todo App v1.0'
+  });
+});
+// GET USER, give user a name and age property
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Kevin',
+    age: 25
+  }, {
+    name: 'Felix',
+    age: 24
+  }, {
+    name: 'Lisa',
+    age: 29
+  }]);
 });
 
 // port we are listening on
 app.listen(3000);
+module.exports.app = app; // accessable from other files
