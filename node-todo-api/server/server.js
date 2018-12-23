742eebb9b49e5f19997854769a447d1c4857b849
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-23 16:58:49 +0100

    added some validators, types and defaults to the model

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -8,28 +8,75 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 // Create a model
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1, // to block empty strings
+    trim: true // gets rid of empty space before and after text
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
+  },
+});
+
+// Create a user
+var User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 1, // to block empty strings
+    trim: true // gets rid of empty space before and after text
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  location: {
+    type: String,
+    default: 'Home'
+  },
+  createdAt: {
+    type: Number,
+    default: null
   },
 });
 
 // var newTodo = new Todo({
 //   text: 'Cook Dinner'
 // });
-var newTodo = new Todo({
-  text: 'Cook Dinner',
-  completed: true,
-  completedAt: 123
+// var newTodo = new Todo({
+//   text: 'Cook Dinner',
+//   completed: true,
+//   completedAt: 123
+// });
+
+// User, email
+var newUser = new User({
+  name: 'Kevin',
+  email: 'kevin@123.com',
+  age: 25,
+  location: 'Zurich',
+  createdAt: 123
 });
 
 // Update Mongodb
-newTodo.save().then((doc) => {
-  console.log('Saved to do', JSON.stringify(doc, undefined, 2));
+// newTodo.save().then((doc) => {
+//   console.log('Saved to do', JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save the Todo');
+// });
+newUser.save().then((doc) => {
+  console.log('Saved User', JSON.stringify(doc, undefined, 2));
 }, (e) => {
-  console.log('Unable to save the Todo');
+  console.log('Unable to save User', e);
 });
