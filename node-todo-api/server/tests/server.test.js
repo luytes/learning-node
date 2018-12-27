bb92f49e91e3b5dd6233fbd635da07702105db54
wenqingkevin ma <wenqing.kevin@gmail.com>
2018-12-27 21:22:12 +0100

    add tests for GET /todos

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -5,13 +5,24 @@ const request = require('supertest');
 const {app} = require('./../server.js');
 // importing Todo from todo.js
 const {Todo} = require('./../models/todo.js');
+// SEEDING
+const todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
 
 // make sure db is empty
 beforeEach((done) => {
   // remove everything
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    // insert many, takes an array an inserts all documents into collection
+    // takes the const todos
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
+// DESCRIBE POST
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
@@ -31,7 +42,7 @@ describe('POST /todos', () => {
           return done(err);
         }
         // find method to fetch everything in that collction (todos)
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           // the todo we created should exist and only length 1 because we only added one
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
@@ -50,9 +61,22 @@ describe('POST /todos', () => {
           return done(err);
         }
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       });
+  });
+});
+
+// DESCRIBE GET
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
