c82420c31faf17ca803125631fca1e5e064f8b92
wenqingkevin ma <wenqing.kevin@gmail.com>
2019-01-07 22:22:32 +0100

    added DELETE /users/me/token route

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -139,6 +139,15 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+// LOGGING OUT
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is up at port ${port}`);
   // tell heroku to "start" in package.json
